8f1b35633f08fc1fea717f998c57724d1903cdeb
Harsh Raj <harshraj8833@gmail.com>
2021-11-01 23:50:33 +0530

    Connection made b/w python and mongoCustomerDatabase

---
+++ python/connect.py
@@ -0,0 +1,9 @@
+import pymongo
+
+if __name__ == "__main__":
+    dbclient = pymongo.MongoClient("mongodb+srv://<harsh92>:<harsh92>@cluster0.ldhgj.mongodb.net/test")
+    
+    db = dbclient["CSEActivity"]
+    collection = db["CustomerDatabase"]
+
+    
