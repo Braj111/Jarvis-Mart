1c7cadd78da03c1abcac26654c6fe96394e69e34
Harsh Raj <harshraj8833@gmail.com>
2021-11-02 00:06:14 +0530

    minor changes(deleted cus_customer.py)

--- cus_formula.py
+++
@@ -1,2 +0,0 @@
-#In this file we are going to create the formula to calculate the Customer score
-
--- python/connect.py
+++ python/connect.py
@@ -5,5 +5,6 @@ if __name__ == "__main__":
     
     db = dbclient["CSEActivity"]
     collection = db["CustomerDatabase"]
+    
 
     
