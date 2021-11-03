9bf412d4ef0cbf4fe9edada30612aa8e914291ec
Harsh Raj <harshraj8833@gmail.com>
2021-11-03 13:31:53 +0530

    Added fetch, add, remove function

--- python/main.py
+++ python/main.py
@@ -1,30 +1,73 @@
 # imports
 import pymongo
-from add_cust import add_cust
+
+# class
+class cust_class(object):
+    def __init__(self, dict):
+        for key in dict:
+            setattr(self, key, dict[key])
 
 
 # function prototypes
 def add_cust():
     name = input("Enter name: ")
     age = int(input("Enter age: "))
+    phone = int(input("Enter phone no.: "))
     amount = int(input("Enter amount: "))
     freq = int(input("Enter frequency: "))
-
-    new_cust = {"Name": name, "Age": age, "Amount": amount, "Frequency": freq, "Customer Score": amount/freq}
-
+    n = collection.estimated_document_count() #Task- Here we have to use some other logic to give the _id to the doc
+    
+    new_cust = {"_id": n+1 ,"Name": name, "Age": age, "Phone": phone, "Amount": amount, "Frequency": freq, "Customer Score": amount/freq}
     collection.insert_one(new_cust)
+
+# remove function -  works fine but there's a clash with _id
+def remove_cust():
+    cust_id = int(input("Enter Customer ID: "))
+    collection.delete_one({"_id": cust_id})
+
+# FUNCTIONS TO FETCH DATA
+def fetch_by_id():
+    cust_id = int(input("Enter Customer ID: "))
+    cust = collection.find_one({"_id": cust_id})
+    customer = cust_class(cust)
+    print(customer.Name)
+
+def fetch_by_phone():
+    phone = int(input("Enter Phone No.: "))
+    cust = collection.find_one({"Phone": phone})
+    customer = cust_class(cust)
+    print(customer.Name)
+
+    
 
 # main 
 if __name__ == "__main__":
+    # connection to db
     client = pymongo.MongoClient("mongodb+srv://harsh92:harsh92@cluster0.ldhgj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
     db = client["g10"]
     collection = db["cust_db"]
 
-    # calling function to add new customer to the data base
-    add_cust()
 
-    # after this I will add another function to update existing customer in the database
+    # calling function to add new customer to the database
+    #add_cust()
+
+
+    # function call to remove customer from the database
+    #remove_cust()
+
+    # function call to fetch doc by id
+    fetch_by_id()
+
+    # funciton call to fetch doc by phone number
+    #fetch_by_phone()
+
+    # Task:-  I will add another function to update existing customer in the database
     # (If someone revisit the amount and frequency will get updated)
 
 
+
+    # Task:- searching data from the database and printing their details
+
     
+
+
