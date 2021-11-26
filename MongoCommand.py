fbc873b3c78ea8ee5ac6bd8d929a750bdeb786f7
Harsh Raj <harsh.raj8833@gmail.com>
2021-11-26 20:51:15 +0530

    Update MongoCommand.py

--- MongoCommand.py
+++ MongoCommand.py
@@ -18,14 +18,13 @@ def add_cust(name,age,phone,amount,freq):
     for i in n:
         for j in i.values():
             num = j
-            print(num)
            
     new_cust = {"_id": num+1, "Name": name, "Age": age, "Phone": phone, "Amount": amount, "Frequency": freq, "Customer Score": amount/freq}
     collection.insert_one(new_cust)
 
 
 
-# remove function -  works fine but there's a clash with _id
+# remove function - 
 def remove_cust(cust_id):
     collection = StartPage.db.cust_db
     collection.delete_one({"_id": cust_id})
